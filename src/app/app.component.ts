3dbb278eaab84a874bffe12cf8fd5421fd83e71a
alberto <alberto.floreso94@gmail.com>
2021-07-03 14:03:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  title = 'Listado de empleados';
+
+  empleados:Empleado[] = [
+    new Empleado("Frida", "Benitez", "Marketing", 3000),
+    new Empleado("Daniela", "Valdéz", "Contadora", 3000),
+    new Empleado("Daniela", "Arizmendi", "Biotecnologa", 3000),
+    new Empleado("Mariza", "Cisneros", "Arquitecta", 3000)
+  ];
 }
